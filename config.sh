19472f232c26e0fba55dfce629115014f5248b22
victortacu5 <83407924+victortacu5@users.noreply.github.com>
2021-05-25 17:47:55 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,6 @@
 git clone https://github.com/victortacu5/Jenkins.git
 sudo -S apt-get install apache2 -y
 sudo systemctl start apache2
-sudo systemctl enable apache2
 sudo cd Jenkins
 sudo cp index.html /var/www/html/
 sudo chmod 777 /var/www/html/index.html
